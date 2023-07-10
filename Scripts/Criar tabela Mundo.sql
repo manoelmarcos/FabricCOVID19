a467cc2b4df850fae17007cf791d37bc1a52b39b
manoelmarcos <42144349+manoelmarcos@users.noreply.github.com>
2023-07-10 20:22:57 -0300

    Delete Criar tabela Mundo.sql

--- Scripts/Criar tabela Mundo.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE [dbo].[bing_covid-19_data_mundo]
-AS
-SELECT DATEPART(YEAR,[updated]) [year], DATEPART(MONTH,[updated]) [month], DATEPART(DAY,[updated]) [dayofmonth], * 
-FROM [dbo].[bing_covid-19_data] 
-WHERE [country_region] = 'Worldwide';
