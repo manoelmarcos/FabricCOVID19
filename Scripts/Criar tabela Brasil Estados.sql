a34e9144377b034a6cae61049e890a24dfa60f58
manoelmarcos <42144349+manoelmarcos@users.noreply.github.com>
2023-07-10 20:19:57 -0300

    Delete Criar tabela Brasil Estados.sql

--- Scripts/Criar tabela Brasil Estados.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE [dbo].[bing_covid-19_data_brasil_estados]
-AS
-SELECT DATEPART(YEAR,[updated]) [year], DATEPART(MONTH,[updated]) [month], DATEPART(DAY,[updated]) [dayofmonth], * 
-FROM [dbo].[bing_covid-19_data] 
-WHERE [country_region] = 'Brazil' and [admin_region_1] is not null;
