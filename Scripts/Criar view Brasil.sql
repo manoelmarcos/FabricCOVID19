0630f089ab22405f0f99988bc330ec69804beb22
manoelmarcos <42144349+manoelmarcos@users.noreply.github.com>
2023-07-20 22:00:41 -0300

    Update Criar view Brasil.sql

--- Scripts/Criar view Brasil.sql
+++ Scripts/Criar view Brasil.sql
@@ -2,4 +2,4 @@ CREATE VIEW [bing_covid-19_data_brasil]
 AS
 SELECT DATEPART(YEAR,[updated]) [year], DATEPART(MONTH,[updated]) [month], DATEPART(DAY,[updated]) [dayofmonth], * 
 FROM [dbo].[bing_covid-19_data] 
-WHERE [iso3] = 'BRA' and [admin_region_1] is not null;
+WHERE [iso3] = 'BRA' and [admin_region_1] is null;
